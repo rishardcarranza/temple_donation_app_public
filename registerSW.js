298e90bd9021e855584a4a0bd5920bf04125501f
Ricardo Carranza <rishardcarranza@gmail.com>
2026-04-29 16:53:24 -0600

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/temple_donation_app_public/sw.js', { scope: '/temple_donation_app_public/' })})}
